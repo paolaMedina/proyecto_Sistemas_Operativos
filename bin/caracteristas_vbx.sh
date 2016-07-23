f65de924fbb9d60c8d55d8dfc0bb8b64ff4414ea
paolaMedina <angie.paola.medina@correounivalle.edu.co>
2016-07-22 22:24:12 -0500

    cambio para que ingrese la maquina consultar
    El cambio se hace por que al subirlo no se callo encuenta que no se mando el script actualizado

--- bin/caracteristas_vbx.sh
+++ bin/caracteristas_vbx.sh
@@ -6,18 +6,18 @@
 
 
 #Se obtiene el numero de Cpus de la maquina virtual y se imprime por pantalla.
-numero_de_CPUs=`VBoxManage showvminfo win | grep "Number of" | tr -s ' ' | cut -d' ' -f4`
+numero_de_CPUs=`VBoxManage showvminfo $1 | grep "Number of" | tr -s ' ' | cut -d' ' -f4`
 echo "Numero de CPUs:" $numero_de_CPUs
 
 #Se obtiene el tamaño de la memoria RAM de la maquina virtual y se imprime por pantalla.
-size_RAM=`VBoxManage showvminfo win | grep Memory | tr -s ' ' | cut -d' ' -f3`
+size_RAM=`VBoxManage showvminfo $1 | grep Memory | tr -s ' ' | cut -d' ' -f3`
 echo "Tamaño de RAM:" $size_RAM
 
 #Se obtiene el numero de interfaces de red de la maquina virtual y se imprime por pantalla.
-num_interfacesRed=`VBoxManage showvminfo win | grep Attachment  | tr -s ' '| tr -d : | cut -d' ' -f2`
+num_interfacesRed=`VBoxManage showvminfo $1 | grep Attachment  | tr -s ' '| tr -d : | cut -d' ' -f2`
 echo "Numero de Interfaces de Red: " $num_interfacesRed
 
 #Se obtiene el tipo de las interfaces de red de la maquina virtual y se imprime por pantalla.
-tipo=`VBoxManage showvminfo win | grep Attachment  | tr -s ' '| tr -d , | cut -d' ' -f6`
+tipo=`VBoxManage showvminfo $1 | grep Attachment  | tr -s ' '| tr -d , | cut -d' ' -f6`
 echo "Tipo:" $tipo
 
